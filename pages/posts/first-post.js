b1ee29ef865f99c535ad6b044f4ce5378c71e392
Srishti Singhal <singhalsrishti98@gmail.com>
2020-08-03 13:04:44 -0700

    creating layout component common to all pages

---
+++ pages/posts/first-post.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import Head from 'next/head'
+
+export default function FirstPost() {
+  return (
+    <>
+      <Head>
+        <title>First Post</title>
+      </Head>  
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  )
+}
