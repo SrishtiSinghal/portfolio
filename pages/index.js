375bb6e4407d890ce58d0ccc0b841b91c61172d2
Srishti Singhal <singhalsrishti98@gmail.com>
2020-08-03 21:52:44 -0700

    changes

--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,10 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm <strong>Srishti </strong>. I am a recent graduate of the University of Victoria
-         with a major in Computer Science and a minor in Business. I have a passion for UX/UI design, problem solving and systems
-         architecture.</p>
-        <p> I have worked as a software developer at a foreign exchange company called <a href = "https://www.globalreachgroup.ca/corporate" >Global Reach</a> where I worked on an international payments
+        <p>Hello, I'm <strong>Srishti. </strong> I am a recent graduate of the University of Victoria
+         with a major in Computer Science and a minor in Business. I love working on 
+         products that improve user experience whether that be a website or an application.</p>
+        <p>I have worked as a software developer at a foreign exchange company called <a href = "https://www.globalreachgroup.ca/corporate" >Global Reach</a> where I worked on an international payments
            software. I have also worked at a SAAS company called <a href = "https://www.letreach.com/">LetReach Technologies</a> where I mostly did front-end development. </p>
         <p>I spend a lot of my time programming and reading technical topics,
            but I also have an interest in business, philosophy and climate change.</p>
