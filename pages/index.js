7765c46a48a7d2f488fc700228f6a1c24559290c
Srishti Singhal <singhalsrishti98@gmail.com>
2020-08-03 21:22:05 -0700

    done editing the description

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,12 @@ export default function Home() {
         <p>Hello, I'm <strong>Srishti </strong>. I am a recent graduate of the University of Victoria
          with a major in Computer Science and a minor in Business. I have a passion for UX/UI design, problem solving and systems
          architecture.</p>
-        <p> I have worked as a software developer at a foreign exchange company called Global Reach Canada where I worked on an international payments
-           software and I have also worked at a SAAS company called LetReach Technologies where I mostly did front-end development. </p>
+        <p> I have worked as a software developer at a foreign exchange company called <a href = "https://www.globalreachgroup.ca/corporate" >Global Reach</a> where I worked on an international payments
+           software. I have also worked at a SAAS company called <a href = "https://www.letreach.com/">LetReach Technologies</a> where I mostly did front-end development. </p>
+        <p>I spend a lot of my time programming and reading technical topics,
+           but I also have an interest in business, philosophy and climate change.</p>
         <p>
-        Feel free to contact me for any reason on <a href= "https://www.linkedin.com/in/srishti98/">LinkedIn </a> or
+          Feel free to contact me for any reason on <a href= "https://www.linkedin.com/in/srishti98/">LinkedIn </a> or
          <a href="mailto:singhalsrishti98@gmail.com"> Email </a>
         </p>
       </section>
